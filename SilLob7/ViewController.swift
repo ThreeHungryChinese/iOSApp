822545ef91e17c1bd7e91a4ec3dcac9616658947
Kefan Zhang <robertmorrislike@gmail.com>
2019-11-28 01:05:29 -0500

    Update student interface and functions

--- SilLob7/ViewController.swift
+++ SilLob7/ViewController.swift
@@ -9,7 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var _username: UITextField!
+    @IBOutlet weak var _password: UITextField!
+    @IBOutlet weak var _login_button: UIButton!
+    @IBOutlet weak var toefl: UITextField!
+    
+    @IBAction func LoginButton(_ sender: Any) {
+        let username = _username.text
+        let password = _password.text
+        if(username == "" || password == ""){
+            return
+        }
+        //DoLogin(username!,password!)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
