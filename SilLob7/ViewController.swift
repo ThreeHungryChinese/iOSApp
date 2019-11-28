9a9e4e64ce42e2a1d68db7745dac8d63585a6a74
Kefan Zhang <robertmorrislike@gmail.com>
2019-11-27 21:00:14 -0500

    Initial Commit

---
+++ SilLob7/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SilLob7
+//
+//  Created by Kefan Zhang on 11/27/19.
+//  Copyright © 2019 Kefan Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SilLob7Tests/SilLob7Tests.swift
@@ -0,0 +1,34 @@
+//
+//  SilLob7Tests.swift
+//  SilLob7Tests
+//
+//  Created by Kefan Zhang on 11/27/19.
+//  Copyright © 2019 Kefan Zhang. All rights reserved.
+//
+
+import XCTest
+@testable import SilLob7
+
+class SilLob7Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
